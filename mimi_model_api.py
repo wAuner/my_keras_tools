d25369677954d709f37306cbc5759b74a5e1d258
peporiz0r <venice@posteo.us>
2017-05-27 16:18:36 +0200

    fixed stuff

--- mimi_model_api.py
+++ mimi_model_api.py
@@ -7,9 +7,12 @@ from keras.optimizers import Adam
 import keras.backend as K
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 
+
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+import itertools
 
 class Mimi:
     """
@@ -17,66 +20,102 @@ class Mimi:
     """
 
 
-    def __init__(self,  workdir=os.getcwd(), train_generator=None, val_generator=None, test_generator=None):
+    def __init__(self,  workdir=os.getcwd(), 
+                 class_names=None, train_generator=None, val_generator=None, test_generator=None):
         
+        """
+        workdir: path to working directory
+        class_names: list of class names in order of the class indices
+        generators: keras generators
+        """
+
         self.train_generator = train_generator
         self.val_generator = val_generator
         self.test_generator = test_generator
         self.workdir = workdir
+
+        if train_generator != None:
+            self.class_names = [0] * len(train_gen.class_indices)
+            for key,value in train_gen.class_indices.items():
+                self.class_names[value] = key
+        if class_names != None:
+            self.class_names = class_names
+
+    def fit(self, X_train=None, y_train=None, save_model=True, plan_learning_rate=False, 
+            batch_size=32, epochs=1, verbose=1, callbacks=None, 
+            validation_split=0.0, validation_data=None, shuffle=True, class_weight=None, 
+            sample_weight=None, initial_epoch=0):
+        """
+        Calls the fit method of the underlying keras model to train on ararys.
+        """
+        callback = self.__get_callbacks(save_model, plan_learning_rate)
+
+        self.model.fit(x=X_train, y=y_train, batch_size=batch_size, epochs=epochs, verbose=verbose, 
+                       callbacks=callback, validation_split=validation_split, validation_data=validation_data, 
+                       shuffle=shuffle, class_weight=class_weight, sample_weight=sample_weight, 
+                       initial_epoch=initial_epoch)
         
     def fit_generator(self, epochs, validate=True, 
                         train_generator=None, val_generator=None, 
-                        divide_steps_per_epoch_by=1, save_model=True):
+                        divide_steps_per_epoch_by=1, save_model=True, plan_learning_rate=False):
         """
         Trains the model using the provided generators. If no generators are provided, it will use the generators provided
         at instanciation.
         """
-        if validate:
-            if train_generator == None:
-                if self.train_generator == None:
-                    raise ValueError("train_generator was not defined at the beginning and none was passed")
-                else:
-                    train_generator = self.train_generator
+        callback = self.__get_callbacks(save_model, plan_learning_rate)
 
+        # if no train_generator was provided, use default one
+        if train_generator == None:
+            if self.train_generator == None:
+                raise ValueError("train_generator was not defined at the beginning and none was passed")
+            else:
+                train_generator = self.train_generator
+
+        # check if training with validation set
+        if validate:
             if val_generator == None:
                 if self.val_generator == None:
                     raise ValueError("val_generator was not defined at the beginning and none was passed")
                 else:
                     val_generator = self.val_generator
 
-            if save_model:
-                if not os.path.isdir(self.workdir+'/models/'):
-                    os.mkdir(self.workdir+'/models/')
+            
 
-                model_saver = ModelCheckpoint(self.workdir+'/models/'+self.name+'.hdf5',save_best_only=True)
-                self.history = self.model.fit_generator(train_generator, 
-                                        steps_per_epoch=train_generator.n / train_generator.batch_size / divide_steps_per_epoch_by,
-                                        epochs=epochs, validation_data=val_generator, 
-                                        validation_steps=val_generator.n / val_generator.batch_size,
-                                        callbacks=[model_saver])
-            else:
-                self.history = self.model.fit_generator(train_generator, 
-                                        steps_per_epoch=train_generator.n / train_generator.batch_size / divide_steps_per_epoch_by,
-                                        epochs=epochs, validation_data=val_generator, 
-                                        validation_steps=val_generator.n / val_generator.batch_size,
-                                        callbacks=[])
+            self.history = self.model.fit_generator(train_generator, 
+                                    steps_per_epoch=train_generator.n / train_generator.batch_size / divide_steps_per_epoch_by,
+                                    epochs=epochs, validation_data=val_generator, 
+                                    validation_steps=val_generator.n / val_generator.batch_size,
+                                    callbacks=callback)
+            
 
         # if validate was set to false, train without using a validation set
         else:
-            if save_model:
-                if not os.path.isdir(self.workdir+'/models/'):
-                    os.mkdir(self.workdir+'/models/')
+            self.history = self.model.fit_generator(train_generator, 
+                                    steps_per_epoch=train_generator.n / train_generator.batch_size / divide_steps_per_epoch_by,
+                                    epochs=epochs, 
+                                    callbacks=callback)
+            
+    def __get_callbacks(self, save_model=True, plan_learning_rate=False):
+        """
+        Creates and returns the callbacks for training.
+        """
+        callback = []
 
-                model_saver = ModelCheckpoint(self.workdir+'/models/'+self.name+'.hdf5',save_best_only=True)
-                self.history = self.model.fit_generator(train_generator, 
-                                        steps_per_epoch=train_generator.n / train_generator.batch_size / divide_steps_per_epoch_by,
-                                        epochs=epochs, 
-                                        callbacks=[model_saver])
-            else:
-                self.history = self.model.fit_generator(train_generator, 
-                                        steps_per_epoch=train_generator.n / train_generator.batch_size / divide_steps_per_epoch_by,
-                                        epochs=epochs,
-                                        callbacks=[])
+        if save_model:  
+            if not os.path.isdir(self.workdir+'/models/'):
+                os.mkdir(self.workdir+'/models/')
+
+            model_saver = ModelCheckpoint(self.workdir+'/models/'+self.name+'.hdf5',save_best_only=True)
+            callback.append(model_saver)
+
+        if plan_learning_rate:
+            # will divide the learning rate by 10 every other epoch
+            lr_planner = LearningRateScheduler(lambda x: self.learning_rate * 0.1 ** (x//2))
+            callback.append(lr_planner)
+
+        return callback
+
+
 
     def predict_generator(self, generator=None):
         """
@@ -93,16 +132,6 @@ class Mimi:
 
         return self.model.predict_generator(generator, steps=generator.n/generator.batch_size,verbose=1)
 
-        
-    def fit(self, X_train=None, y_train=None, batch_size=32, epochs=1, verbose=1, callbacks=None, validation_split=0.0, 
-            validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0):
-        """
-        Calls the fit method of the underlying keras model to train on ararys.
-        """
-        self.model.fit(X_train=X_train, y_train=y_train, batch_size=batch_size, epochs=epochs, verbose=verbose, 
-                       callbacks=callbacks, validation_split=validation_split, validation_data=validation_data, 
-                       shuffle=shuffle, class_weight=class_weight, sample_weight=sample_weight, 
-                       initial_epoch=initial_epoch)
 
     def predict(self, x, batch_size=32, verbose=0):
         """
@@ -118,11 +147,18 @@ class Mimi:
         self.model.save(path+self.name+'.hdf5')
         print('Model has been saved at ',path+self.name+'.hdf5')
 
+    def summary(self):
+        self.model.summary()
 
-    def evaluate_predictions(self, generator=None, show_confunsion_matrix=True):
+    def compile(self, optimizer, loss, metrics=None, loss_weights=None, sample_weight_mode=None):
+        self.model.compile(self, optimizer=optimizer, loss=loss, metrics=metrics, loss_weights=loss_weights, 
+                            sample_weight_mode=sample_weight_mode)
+
+
+    def evaluate_predictions_gen(self, generator=None, show_confunsion_matrix=True):
         """
         Makes predictions on generator and returns indices of correct and incorrect classified images.
-        Default behavior is predicting on validation generator
+        Default behavior is predicting on validation generator. Also plots confusion matrix.
         """
         if generator == None:
             if self.val_generator == None:
@@ -131,21 +167,54 @@ class Mimi:
                 generator = self.val_generator
         predictions = self.predict_generator(generator)
 
-        correct_preds = np.where(np.argmax(predictions, axis=1)==generator.classes)[0]
-        wrong_preds = np.where(np.argmax(predictions, axis=1)!=generator.classes)[0]
+        correct_preds = np.where(np.argmax(predictions, axis=1) == generator.classes)[0]
+        wrong_preds = np.where(np.argmax(predictions, axis=1) != generator.classes)[0]
         
+        if show_confunsion_matrix:
+            # getting class_names from generator
+            class_names = [0] * len(train_gen.class_indices)
+            for key,value in train_gen.class_indices.items():
+                class_names[value] = key
+
+            conf_matrix = confusion_matrix(generator.classes, np.argmax(predictions, axis=1))
+            self.plot_confusion_matrix(conf_matrix, self.class_names, title='Confusion matrix, without normalization')
+
+        return correct_preds, wrong_preds
+
+    def evaluate_predictions_arr(self, X,  y_true, class_names=None, show_confunsion_matrix=True, batch_size=32):
+        """
+        Makes predictions on array X and returns indices of correct and incorrect classified images.
+        Also plots confusion matrix.
+
+        y_true: array with the correct labels
+        class_names: list of strings with class names
+        """
+        # if labels are one_hot_encoded, decode them
+        if y_true.ndim > 1 and y_true.shape[1] > 1:
+            y_true = np.argmax(y_true, axis=1)
+            print("Labels have been decoded.")
+
+        # if no class names were provided, try to find predefined names
+        if class_names == None:
+            class_names = self.class_names
+
+        predictions = self.predict(X, batch_size=batch_size)
+
+        correct_preds = np.where(np.argmax(predictions, axis=1) == y_true)[0]
+        wrong_preds = np.where(np.argmax(predictions, axis=1) != y_true)[0]
         
-        class_names = [name for name in generator.class_indices.keys()]
-        conf_matrix = confusion_matrix(generator.classes, np.argmax(predictions, axis=1))
-        self.plot_confusion_matrix(conf_matrix, class_names, title='Confusion matrix, without normalization')
+        if show_confunsion_matrix:
+            conf_matrix = confusion_matrix(y_true, np.argmax(predictions, axis=1))
+            self.plot_confusion_matrix(conf_matrix, class_names, title='Confusion matrix, without normalization')
 
         return correct_preds, wrong_preds
 
 
-    def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    def plot_confusion_matrix(self, cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
         """
         This function prints and plots the confusion matrix.
         Normalization can be applied by setting `normalize=True`.
+        cm: confusion_matrix
         """
         plt.imshow(cm, interpolation='nearest', cmap=cmap)
         plt.title(title)
